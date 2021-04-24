2fd45429476803e64ea1b45d6c99c00fe9da7a14
Uzytkownik <uzytkownik@kursGIT.pl>
2021-04-24 11:21:40 +0200

    Początkowe ustawienia + API

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("http://api.open-notify.org/iss-now.json");
+        URLConnection urlcon = url.openConnection();
+
+        InputStream is = urlcon.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+    }
+}
