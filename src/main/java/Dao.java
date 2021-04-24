cd87b92084187069185c89c62a99e1b4fa4c5728
Oskar Woźniak <oskar.tomasz.wozniak@gmail.com>
2021-04-24 12:21:08 +0200

    Otwarcie polaczenia bazy danych i zamykanie go

--- src/main/java/Dao.java
+++ src/main/java/Dao.java
@@ -1,7 +1,49 @@
 import dao.IDao;
 import model.ISSPosition;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Dao implements IDao {
+
+    private String databaseUrl = "";
+    private Connection connection;
+
+    private void openConnection(){
+        try {
+            connection = DriverManager.getConnection(databaseUrl, "root", "example12345");
+            System.out.println("Database Connected");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void closeConnection(){
+        if (connection != null){
+            try {
+                connection.close();
+                System.out.println("Database connection closed");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private int update(String input){
+        openConnection();
+        int result = 1;
+        try {
+            Statement statement = connection.createStatement();
+            result = statement.executeUpdate(input);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        closeConnection();
+        return result;
+    }
+
     @Override
     public String getCurrentIssPosition() {
         return null;
@@ -9,11 +51,15 @@ public class Dao implements IDao {
 
     @Override
     public ISSPosition addIssPosition(ISSPosition issPosition) {
+//         Wprowadzenie danych do bazy danych.  Latitude(String), Longitude(String), timestamp(sekundy, long),
+//        Data dodania rekordu(LocalDate), Liczba czlonkow zalogi(int), predkosc poruszania sie stacji(int)
+        String insertIssPosition = String.format("INSERT INTO ()");
         return null;
     }
 
     @Override
     public int addIssSpeed() {
+
         return 0;
     }
 
