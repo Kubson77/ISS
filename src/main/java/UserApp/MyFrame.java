44c57e2245f57cc861a0b96b04d6580221c4b746
Oskar Woźniak <oskar.tomasz.wozniak@gmail.com>
2021-05-08 10:38:40 +0200

    Astr #

--- src/main/java/UserApp/MyFrame.java
+++ src/main/java/UserApp/MyFrame.java
@@ -25,23 +25,23 @@ class ButtonPanel extends JPanel implements ActionListener {
     public static final int WIDTH = 1500;
     Dao dao = new Dao();
     private JButton getSpeedButton;
-    private JButton redButton;
+    private JButton astronautsNum;
     private JButton locationButton;
 
     public ButtonPanel() {
         locationButton = new JButton("Last Location");
         getSpeedButton = new JButton("Speed of ISS");
-        redButton = new JButton("Red");
+        astronautsNum = new JButton("Astronauts Number");
 
         locationButton.addActionListener(this);
         getSpeedButton.addActionListener(this);
-        redButton.addActionListener(this);
+        astronautsNum.addActionListener(this);
 
         setLayout(new GridLayout());
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         add(locationButton);
         add(getSpeedButton);
-        add(redButton);
+        add(astronautsNum);
     }
 
     @Override
@@ -53,11 +53,13 @@ class ButtonPanel extends JPanel implements ActionListener {
                     + dao.getLastIssCoordinates().getLatitude());
             JOptionPane.showMessageDialog(null, msg);
         } else if (source == getSpeedButton) {
-            String speedMsg = String.format("Speed: " + dao.getLastIssCoordinates().getSpeed() + "km/h");
+            String speedMsg = String.format("Speed: " + dao.getLastIssCoordinates().getSpeed() + " km/h");
             JOptionPane.showMessageDialog(null, speedMsg);
         }
-        else if (source == redButton) {
-            setBackground(Color.RED);
+        else if (source == astronautsNum) {
+            String astronautsMsg = String.format("Astronauts: " + dao.getHowManyPeopleInIss().toString());
+            dao.getAstronauts();
+            JOptionPane.showMessageDialog(null, astronautsMsg);
         }
     }
 }
